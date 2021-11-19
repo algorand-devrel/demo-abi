0f8425f700bebbfe832dbca5f13d9414831b975c
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-11-19 11:17:09 -0500

    add msig

---
+++ py/msig.py
@@ -0,0 +1,24 @@
+from algosdk.v2client.algod import AlgodClient 
+from algosdk.future.transaction import Multisig, MultisigTransaction
+from algosdk.account import generate_account
+from algosdk.future.transaction import PaymentTxn
+from algosdk.future.atomic_transaction_composer import MultisigTransactionSigner, AtomicTransactionComposer, TransactionWithSigner
+
+client = AlgodClient("a"*64, "http://localhost:4001")
+
+sk1, addr1 = generate_account()
+sk2, addr2 = generate_account()
+
+msig = Multisig(1, 2, [addr1, addr2])
+
+sp = client.suggested_params()
+
+comp = AtomicTransactionComposer()
+comp.add_transaction(
+    TransactionWithSigner(
+        MultisigTransaction(PaymentTxn(msig.address(), sp, addr1, 100000), msig),
+        MultisigTransactionSigner([sk1,sk2])
+    )
+)
+
+print(comp.execute(client, 2))
